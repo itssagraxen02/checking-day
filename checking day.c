001a0a9736d18da2d072bdd8d3d8c57cbae9667c
itssagraxen02 <agrasenc2017@gmail.com>
2024-12-23 13:11:38 +0530

    Add files via upload

---
+++ checking day.c
@@ -0,0 +1,49 @@
+# include<stdio.h>
+int main(){
+
+int day;
+printf("enter day (1-7)");
+scanf("%d", &day);
+
+switch (day)
+{
+case 1 : printf("Monday /n");
+    break;
+case 2 :printf("tuesday /n");
+    break;
+case 3 :printf("wednesday /n");
+    break;
+case 4 :printf("thursday /n");
+    break;
+case 5 :printf("fridayday /n");
+    break;
+case 6 :printf("saturday /n");
+    break;
+case 7 :printf("sunday /n");
+    break;
+
+
+default: printf("not valid");
+
+return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
